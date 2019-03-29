8672b8d2c58f45d6dcb872831f420405b82b770b
Damien Laurens <damien.laurens.ext@orange.com>
2019-03-29 14:18:05 +0100

    Initialisation

---
+++ src/main/java/com/coderoyale/classes/Building.java
@@ -0,0 +1,15 @@
+package com.coderoyale.classes;
+
+public class Building {
+    private int structureType;
+    private int owner;
+
+
+    public int getStructureType() {
+        return structureType;
+    }
+
+    public void setStructureType(int structureType) {
+        this.structureType = structureType;
+    }
+}
